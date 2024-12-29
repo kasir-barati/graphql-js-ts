a03509af2568c9bbbfc6ebc12eee6e60225748b4
Kasir Barati <kasir.barati@gmail.com>
2024-12-29 19:43:35 +0100

    chore: init depth e2e tests

---
+++ apps/depth-e2e/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'jest';
+
+export default {
+  displayName: 'depth-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/depth-e2e',
+} satisfies Config;
