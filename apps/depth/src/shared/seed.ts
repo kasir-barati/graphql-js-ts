1a0afe1d8a35083b9d5ae8e8fbe123d91476180e
Kasir Barati <kasir.barati@gmail.com>
2024-12-29 20:03:12 +0100

    chore: seed depth with dummy data

---
+++ apps/depth/src/shared/seed.ts
@@ -0,0 +1,46 @@
+import { BusinessEntity } from '../business/business.entity';
+import { CustomerEntity } from '../customer/customer.entity';
+import { AppDataSource } from './data-source';
+
+(async () => {
+  await AppDataSource.initialize();
+
+  const businessRepository =
+    AppDataSource.getRepository(BusinessEntity);
+  const customerRepository =
+    AppDataSource.getRepository(CustomerEntity);
+
+  if ((await businessRepository.count()) > 0) {
+    return 'Was seeded!';
+  }
+
+  const businesses = new Array<Partial<BusinessEntity>>(100)
+    .fill({})
+    .map((_, index) => {
+      return {
+        name: 'business ' + index + 1,
+      } satisfies Partial<BusinessEntity>;
+    });
+  const createdBusinesses = await businessRepository.save(businesses);
+  const customers: Partial<CustomerEntity>[] = [];
+
+  for (const business of createdBusinesses) {
+    customers.push(
+      ...new Array<Partial<CustomerEntity>>(100)
+        .fill({})
+        .map((_, index) => {
+          return {
+            name: 'customer' + index + 1,
+            shopAtId: business.id,
+          } satisfies Partial<CustomerEntity>;
+        }),
+    );
+  }
+
+  await customerRepository.save(customers);
+  await AppDataSource.destroy();
+
+  return 'Seeded!';
+})()
+  .then(console.log)
+  .catch(console.error);
